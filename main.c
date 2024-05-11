ce1cd7f535f6e4f67f99482bd4035f59c111b657
keyminy <imanaifs@gmail.com>
2024-05-11 16:00:59 +0900

    초,분 변경만 남음

--- main.c
+++ main.c
@@ -39,10 +39,10 @@ typedef struct{
 volatile Stopwatch *main_stopwatch_ptr;
 volatile Min2Sec_Clock *main_min2secClock_ptr;
 
-void stop_watch_state(Stopwatch* pStopwatch);
-void fnd_display(Stopwatch* pStopwatch);
+void stop_watch_state(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock);
+void min2sec_clock_state(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock);
+void fnd_display(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock);
 void init_timer0();
-
 
 //timer0를 초기화 한다.
 void init_timer0(){
@@ -59,20 +59,33 @@ ISR(TIMER0_OVF_vect){
 	/* 인터럽트 루틴을 가능한 짧게 짜라, ms_count만 증가시키고 빠져나오게함 */
 	TCNT0=6; // 6 ~ 256개의 pulse카운트 --> 1ms를 맞춰주기 위해서 TCNT0을 6으로 설정
 	main_stopwatch_ptr->display_count++;
+	main_min2secClock_ptr->display_count++;
 	if(main_stopwatch_ptr->state == STOPWATCH_RUNNING){
 		main_stopwatch_ptr->ms_count++;
+	}
+	if(main_min2secClock_ptr->state == CLOCK_RUNNING){
+		main_min2secClock_ptr->ms_count++;
 	}
 }
 
 
 int main(void)
 {
+	/* initialize stopwatch */
 	// display_count = 0;
 	// ms_count = 0;
 	// sec_count = 0;
-	// state = IDLE
+	// state = STOPWATCH_IDLE
     Stopwatch stopwatch = {0,0,0,STOPWATCH_IDLE};
 	main_stopwatch_ptr = &stopwatch;
+	
+	/* initialize min2sec clock */
+	// display_count = 0;
+	// ms_count = 0;
+	// sec_count = 0;
+	// state = CLOCK_IDLE
+	Min2Sec_Clock min2sec_clock = {0,0,0,CLOCK_IDLE};
+	main_min2secClock_ptr = &min2sec_clock;
 	
 	init_fnd();
 	init_button();
@@ -80,20 +93,54 @@ int main(void)
 	
     while (1) 
     {
-		stop_watch_state(&stopwatch);
-		fnd_display(&stopwatch);
+		stop_watch_state(&stopwatch,&min2sec_clock);
+		min2sec_clock_state(&stopwatch,&min2sec_clock);
+		fnd_display(&stopwatch,&min2sec_clock);
     }
 }
 
-void stop_watch_state(Stopwatch* pStopwatch){
-	switch(pStopwatch->state){
-		case STOPWATCH_IDLE:
+void min2sec_clock_state(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock){
+	if(pStopwatch->state == STOPWATCH_IDLE && pMin2sec_clock->state!= CLOCK_IDLE){
+		switch (pMin2sec_clock->state){
+			case CLOCK_RUNNING:
+				// increase clock time
+				if(pMin2sec_clock->ms_count >= 1000){
+					pMin2sec_clock->ms_count = 0;
+					pMin2sec_clock->sec_count++;
+					if(main_min2secClock_ptr->sec_count == 600){
+						// 10분이되면 초기화
+						main_min2secClock_ptr->sec_count = 0;
+					}
+				}
+				// Back to stopwatch mode
+				if(get_button(BUTTON1_PIN,BUTTON1)) {
+					pStopwatch->state = STOPWATCH_RUNNING;
+					pMin2sec_clock->state = CLOCK_IDLE;
+				}
+				// change second
+				else if(get_button(BUTTON2_PIN,BUTTON2)) pMin2sec_clock->state = CHANGE_SEC;
+				break;
+			case CHANGE_SEC:
+				break;
+			case CHANGE_MIN:
+				break;
+		}
+	}
+	return;
+}
+
+void stop_watch_state(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock){
+	if(pMin2sec_clock->state == CLOCK_IDLE){
+		switch(pStopwatch->state){
+			case STOPWATCH_IDLE:
 			pStopwatch->ms_count = 0;
 			pStopwatch->sec_count = 0;
 			FND_DIGIT_PORT = 0xff;
-			if(get_button(BUTTON1_PIN,BUTTON1)) pStopwatch->state = STOPWATCH_RUNNING;
+			if(get_button(BUTTON1_PIN,BUTTON1)) {
+				pStopwatch->state = STOPWATCH_RUNNING;
+			}
 			break;
-		case STOPWATCH_RUNNING:
+			case STOPWATCH_RUNNING:
 			if(pStopwatch->ms_count >= 1000){
 				pStopwatch->ms_count = 0;
 				pStopwatch->sec_count++;
@@ -102,19 +149,32 @@ void stop_watch_state(Stopwatch* pStopwatch){
 					main_stopwatch_ptr->sec_count = 0;
 				}
 			}
-			if(get_button(BUTTON1_PIN,BUTTON1)) pStopwatch->state = STOPWATCH_PAUSED;
-			else if(get_button(BUTTON2_PIN,BUTTON2)) pStopwatch->state = STOPWATCH_IDLE;
+			// Switch to Min2Sec_Clock
+			if(get_button(BUTTON1_PIN,BUTTON1)) {
+				pStopwatch->state = STOPWATCH_IDLE;
+				pMin2sec_clock->state = CLOCK_RUNNING;
+			}
+			else if(get_button(BUTTON2_PIN,BUTTON2)) pStopwatch->state = STOPWATCH_PAUSED;
 			break;
-		case STOPWATCH_PAUSED:
-			if(get_button(BUTTON1_PIN,BUTTON1)) pStopwatch->state = STOPWATCH_RUNNING;
-			else if(get_button(BUTTON2_PIN,BUTTON2)) pStopwatch->state = STOPWATCH_IDLE;
+			case STOPWATCH_PAUSED:
+			// Switch to Min2Sec_Clock
+			if(get_button(BUTTON1_PIN,BUTTON1)) {
+				pStopwatch->state = STOPWATCH_IDLE;
+				pMin2sec_clock->state = CLOCK_RUNNING;
+			}
+			else if(get_button(BUTTON2_PIN,BUTTON2)) pStopwatch->state = STOPWATCH_RUNNING;
+			else if(get_button(BUTTON3_PIN,BUTTON3)) {
+				pStopwatch->state = STOPWATCH_IDLE;
+				pMin2sec_clock->state = STOPWATCH_IDLE;
+			};
 			break;
+		}
 	}
+	return;
 }
 
-
 // display the FND
-void fnd_display(Stopwatch* pStopwatch){
+void fnd_display(Stopwatch* pStopwatch, Min2Sec_Clock* pMin2sec_clock){
 	if(pStopwatch->state != STOPWATCH_IDLE){
 		// 0을 찍을려면 g빼고 1로 16진수값 3F입니다, b는 0011_1111(common anode)
 		// common cathode는 1100_0000이고, 16진수로 C0
@@ -144,5 +204,31 @@ void fnd_display(Stopwatch* pStopwatch){
 		digit_position = pStopwatch->display_count; // digit_position = stopwatch->display_count;
 		digit_position %= 8; // 다음 표시할 자리수를 준비하고 함수 종료
 	}	
-}
+	else if(pMin2sec_clock->state != CLOCK_IDLE){
+		// shows fnd when in clock mode
+		unsigned char fnd_font[] = {~0xc0, ~0xf9, ~0xa4, ~0xb0,~0x99,~0x92,~0x82,~0xd8,~0x80,~0x98,~0x7f};
 
+		static int digit_position = 0; // 자리수 선택  변수 0~3 : 0,1,2,3
+
+		switch(digit_position){
+			case 0: // 1단위 : 1s마다(0~9)
+			FND_DIGIT_PORT = ~0b10000000; // cathode
+			FND_DATA_PORT = fnd_font[pMin2sec_clock->sec_count%10];
+			break;
+			case 2: // 10단위 : 10단위s마다(0~6)
+			FND_DIGIT_PORT = ~0b01000000; // cathode
+			FND_DATA_PORT =fnd_font[pMin2sec_clock->sec_count/10%6];
+			break;
+			case 4: // 100단위 : 분단위=60초마다(0~9)
+			FND_DIGIT_PORT = ~0b00100000; // cathode
+			FND_DATA_PORT = fnd_font[pMin2sec_clock->sec_count/60%10];
+			break;
+			case 6: // 1000단위 : 10단위분마다=600초마다(0~6)
+			FND_DIGIT_PORT = ~0b00010000; // cathode
+			FND_DATA_PORT = fnd_font[pMin2sec_clock->sec_count/600%6];
+			break;
+		}
+		digit_position = pMin2sec_clock->display_count; // digit_position = stopwatch->display_count;
+		digit_position %= 8; // 다음 표시할 자리수를 준비하고 함수 종료
+	}
+}
